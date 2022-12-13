fe22db42bb72c826e3d229f236ad7f5069679f50
Isaac Holt <isaac_holt@icloud.com>
2022-12-13 00:12:36 +0000

    Pls

--- replit.nix
+++ replit.nix
@@ -2,17 +2,4 @@
   deps = [
 	pkgs.python310
 ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Needed for pandas / numpy
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      # Needed for pygame
-      pkgs.glib
-      # Needed for matplotlib
-      pkgs.xorg.libX11
-    ];
-    PYTHONBIN = "${pkgs.python38Full}/bin/python3.10";
-    LANG = "en_US.UTF-8";
-  };
 }
